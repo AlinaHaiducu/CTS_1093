e942ba98894190c1295f8789250eea38df7828c8
alina <haiducualina18@stud.ase.ro>
2021-04-22 15:21:53 +0300

    Flyweight Implementation

---
+++ Week9 - Composite Flyweight/src/ro/ase/csie/cts/g1093/dp/flyweight/ModelsFactory.java
@@ -0,0 +1,20 @@
+package ro.ase.csie.cts.g1093.dp.flyweight;
+
+import java.util.HashMap;
+
+public class ModelsFactory {
+	
+	static HashMap<String, Model3DInterface> models = new HashMap<>();
+	
+	static {
+		models.put(ModelType.BUIDLING.toString(), new Model3DFlyweight("Building"));
+		models.put(ModelType.SOLDIER.toString(), new Model3DFlyweight("Soldier"));
+	}
+	
+	public static Model3DInterface getModel(ModelType type) {
+		return models.get(type.toString());
+	}
+	
+	
+
+}
